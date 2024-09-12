8a7e4d8d48821d5b01fd46cfa9bf1027820ada5e
Marija Vrljanac <marija.vrljanac@gmail.com>
2024-09-12 16:52:42 +0200

    retrieving data with http and observables done

--- apm-begin/src/app/app.config.ts
+++ apm-begin/src/app/app.config.ts
@@ -1,16 +1,18 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { provideRouter } from '@angular/router';
 import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { AppData } from './app-data';
-import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     importProvidersFrom(
       FormsModule,
-      InMemoryWebApiModule.forRoot(AppData, { delay: 1000 })
+      InMemoryWebApiModule.forRoot(AppData, { delay: 1000 }),
+      HttpClientModule
     ),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+  ],
 };
--- apm-begin/src/app/products/product-list/product-list.component.ts
+++ apm-begin/src/app/products/product-list/product-list.component.ts
@@ -1,25 +1,43 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 
-import { NgIf, NgFor, NgClass } from '@angular/common';
+import { NgClass, NgFor, NgIf } from '@angular/common';
+import { Subscription, tap } from 'rxjs';
 import { Product } from '../product';
 import { ProductDetailComponent } from '../product-detail/product-detail.component';
+import { ProductService } from '../product.service';
 
 @Component({
-    selector: 'pm-product-list',
-    templateUrl: './product-list.component.html',
-    standalone: true,
-  imports: [NgIf, NgFor, NgClass, ProductDetailComponent]
+  selector: 'pm-product-list',
+  templateUrl: './product-list.component.html',
+  standalone: true,
+  imports: [NgIf, NgFor, NgClass, ProductDetailComponent],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit, OnDestroy {
   // Just enough here for the template to compile
   pageTitle = 'Products';
   errorMessage = '';
+  sub!: Subscription;
+
+  private productService = inject(ProductService);
 
   // Products
   products: Product[] = [];
 
   // Selected product id to highlight the entry
   selectedProductId: number = 0;
+
+  ngOnInit(): void {
+    this.sub = this.productService
+      .getProducts()
+      .pipe(tap(() => console.log('In component pipeline')))
+      .subscribe((products) => {
+        this.products = products;
+        console.log(this.products);
+      });
+  }
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
+  }
 
   onSelected(productId: number): void {
     this.selectedProductId = productId;
